b2b685323d5b3dd3ffddb9d98c3ea22dfd46f7b0
anandtanuku <tanandkumar369@gmail.com>
2026-05-02 20:19:20 +0530

    fix db connection

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "postgresql://postgres:Anand%40358@localhost:5432/product_db"
+DATABASE_URL = os.getenv("DATABASE_URL") or "postgresql+psycopg2://neondb_owner:npg_PEm6MUJ0Xdqz@ep-aged-scene-anlvbeiq.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require"
 engine = create_engine(DATABASE_URL)
 
 session = sessionmaker(autocommit = False, autoflush = False, bind=engine)
-
