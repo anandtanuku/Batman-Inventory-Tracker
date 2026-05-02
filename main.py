23a6d514a6a091268457e0e23612e753d257dc6c
anandtanuku <tanandkumar369@gmail.com>
2026-05-02 20:43:52 +0530

    fix cors for production

--- main.py
+++ main.py
@@ -11,8 +11,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["http://localhost:3000"],
-    allow_methods = ["*"]
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 def get_db():
